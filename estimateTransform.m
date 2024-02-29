8e3f4adb252f3ef89b900096c4a3bd4d795e417e
Layan Alnamlah <90392216+layan00@users.noreply.github.com>
2024-02-28 21:35:08 -0500

    Update estimateTransform.m

--- estimateTransform.m
+++ estimateTransform.m
@@ -3,13 +3,19 @@
 
 function A = estimateTransform(im1_points, im2_points)
 
-% 1) we know that Pq=r, create P and r using DLT %
+% Extract coordinates
+x = im1_points(:, 1);
+y = im1_points(:, 2);
+xp = im2_points(:, 1);
+yp = im2_points(:, 2);
 
-% Set up design matrix P: size = 2*size(im1_points,1) x 9
-% P =zeros(2*size(im1_points,1),9); not sure
-
-% 2) use Homogeneous Least Squares with SVD method to to obtain q %
-
+% Construct the design matrix P
+P = [
+    -x, -y, -ones(size(x)), zeros(size(x)), zeros(size(x)), zeros(size(x)), x .* xp, y .* xp, xp;
+    zeros(size(x)), zeros(size(x)), zeros(size(x)), -x, -y, -ones(size(x)), x .* yp, y .* yp, yp
+];
+    
+% Use Homogeneous Least Squares with SVD to obtain q
 if size(P,1) == 8
     [U,S,V] = svd(P);
 else
@@ -17,8 +23,8 @@ else
 end
 
 q = V(:,end);
-
-% 3) reshape q to get A %
-
-
+    
+    % Reshape q to obtain the transformation matrix A
+    A = reshape(q, 3, 3)';
+    disp(A);
 end
