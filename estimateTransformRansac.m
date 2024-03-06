9b990bb8512b727d277c88c0f203089556464562
Layan Alnamlah <90392216+layan00@users.noreply.github.com>
2024-03-06 00:09:24 -0500

    comments estimateTransformRansac.m

--- estimateTransformRansac.m
+++ estimateTransformRansac.m
@@ -1,11 +1,16 @@
+% The following function estimates transformation by following Ransac 
+% which tests random points to increase accuracy 
+
 function A_inliers = estimateTransformRansac(pts1, pts2, im1, im2)
-    Nransac = 10000;
-    t = 2;
-    n = size(pts1, 1);
-    k = 4;
+    Nransac = 10000; % set for the sake of speed and accuracy
+    t = 2; % Error threshold
+    n = size(pts1, 1); % size of corresponding points
+    k = 4; % minimum number of points for homography
+    % Define the best data
     nbest = 0;
     Abest = [];
     idxbest = [];
+    % For loop to find best data and calls estimateTransform
     for i_ransac = 1:Nransac
         idx = randperm(n, k);
         pts1i = pts1(idx, :);
@@ -23,14 +28,15 @@ function A_inliers = estimateTransformRansac(pts1, pts2, im1, im2)
             idxbest = idxgood;
         end
     end
-
+    % Create inliers
     pts1inliers = pts1(idxbest, :);
     pts2inliers = pts2(idxbest, :);
-
+    
+    % Visualize corresponding points between the two images 
     figure();
     showMatchedFeatures(im1, im2, pts1inliers, pts2inliers, "montage");
     title("Refined matching points (by RANSAC), only inliers");
-    saveas(gcf, 'matched_features_plot.png');
+    saveas(gcf, 'matched_features_plot.png'); % save as png for revision
 
     A_inliers = estimateTransform(pts1inliers, pts2inliers);
 end
